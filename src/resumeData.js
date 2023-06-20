c52bba999954b2038de1fda0427b160668021e75
iro tochukwu samuel <irotochukwu@gmail.com>
2023-06-20 07:17:54 +0100

    Initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,103 @@
+let resumeData = {
+    "imagebaseurl": "https://rbhatia46.github.io/",
+    "name": "Iro Tochukwu",
+    "role": "Software Engineer",
+    "linkedinId": "https://www.linkedin.com/in/iro-tochukwu-samuel-a91789162/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of software development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks": [
+        {
+            "name": "linkedin",
+            "url": "https://www.linkedin.com/in/iro-tochukwu-samuel-a91789162/",
+            "className": "fa fa-linkedin"
+        },
+        {
+            "name": "github",
+            "url": "http://github.com/irotochukwusamuel",
+            "className": "fa fa-github"
+        },
+        {
+            "name": "skype",
+            "url": "https://twitter.com/clairclancy",
+            "className": "fa fa-twitter"
+        }
+    ],
+    "aboutme": "I am a computer engineering graduate with over 6 years of experience in software development. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address": "Nigeria",
+    "education": [
+        {
+            "UniversityName": "Abia State Polytechnic, Aba",
+            "specialization": "Computer Engineering Technology",
+            "MonthOfPassing": "Jan",
+            "YearOfPassing": "2019",
+            "Achievements": "Higher National Diploma"
+        }
+    ],
+    "work": [
+        {
+            "CompanyName": "UMBA BANK, San Fransisco",
+            "specialization": "Senior Software Engineer",
+            "YearOfLeaving": "Current",
+            "Achievements": "Built and developed a payroll application system which generated great revenue for the company and also offered good services to big companies allowing them automate their employee’s salary and also allow such companies have access to large loan. The application makes use of React for the frontend and Python for the backend."
+        },
+        {
+            "CompanyName": "3kle Bank, United Kingdom",
+            "specialization": "Senior Software Engineer",
+            "MonthOfLeaving": "Dec",
+            "YearOfLeaving": "2021",
+            "Achievements": "Built and developed 70% of the company services and features including flight booking, budget spending, send money etc using AWS cloud services"
+        },
+        {
+            "CompanyName": "Hourspent, Nigeria",
+            "specialization": "Software Engineer",
+            "MonthOfLeaving": "Jun",
+            "YearOfLeaving": "2021",
+            "Achievements": "Redesigned company's web-based application with HTML,CSS,JavaScript,Python and provided beneficial IT support to colleagues and clients and also developed new software    functionality such as an Event planning IOS app using Swift language and Python Django, ensuring that client's vision and requirements were met."
+        }
+    ],
+    "skillsDescription": "skills",
+    "skills": [
+        {
+            "skillname": "HTML5"
+        },
+        {
+            "skillname": "CSS"
+        },
+        {
+            "skillname": "Reactjs"
+        },
+        {
+            "skillname": "Python"
+        },
+        {
+            "skillname": "FLASK"
+        },
+        {
+            "skillname": "AWS"
+        },
+        {
+            "skillname": "MYSQL"
+        },
+        {
+            "skillname": "POSTGRESQL"
+        },
+        {
+            "skillname": "DJANGO"
+        },
+        {
+            "skillname": "FASTAPI"
+        }
+    ],
+    "testimonials": [
+        {
+            "description": "This is a sample testimonial",
+            "name": "Some technical guy"
+        },
+        {
+            "description": "This is a sample testimonial",
+            "name": "Some technical guy"
+        }
+    ]
+}
+
+export default resumeData
